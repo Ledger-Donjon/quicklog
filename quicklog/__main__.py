0b064248b47639f64e54b2feafc60715b19f8c61
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2023-02-02 17:07:31 +0100

    Updated info command for batches
    quicklog info command now takes into account batch files in the report.

--- quicklog/__main__.py
+++ quicklog/__main__.py
@@ -15,22 +15,41 @@ def info(log):
     total_size = 0
     trace_count = 0
     existing = 0
+    existing_batch_file_count = 0
+    batches_already_seen = set()
     lines = log.readlines()
     for line in progressbar.progressbar(lines):
         record = json.loads(line)
-        rid = record["id"]
-        if is_valid_rid(rid):
-            trace_count += 1
-            trace_path = get_trace_path(rid)
-            try:
-                size = os.path.getsize(trace_path)
-                total_size += size
-                existing += 1
-            except OSError as e:
-                pass
+        if "bid" in record:
+            # Trace file saved in batch
+            bid = record["bid"]
+            if is_valid_rid(bid):
+                bid = bytes.fromhex(bid)
+                trace_count += 1
+                if bid not in batches_already_seen:
+                    batches_already_seen.add(bid)
+                    try:
+                        size = os.path.getsize(get_trace_path(bid))
+                        total_size += size
+                        existing_batch_file_count += 1
+                    except OSError:
+                        pass
+        else:
+            rid = record["id"]
+            if is_valid_rid(rid):
+                rid = bytes.fromhex(rid)
+                trace_count += 1
+                trace_path = get_trace_path(rid)
+                try:
+                    size = os.path.getsize(trace_path)
+                    total_size += size
+                    existing += 1
+                except OSError as e:
+                    pass
     print(f"{len(lines)} records")
     print(f"{trace_count} traces")
     print(f"{existing} trace files found")
+    print(f"{existing_batch_file_count} batch files found")
     size_gb = total_size / (1024**3)
     print(f"{size_gb:.3f} GB in traces database")
 
